10464adc97624cce6a083622db6b3143c0759b12
Remy Vercampst <r.vercampst@wavenet.be>
2023-06-22 11:02:19 +0200

    feat(Drawer.tsx): add Drawer component with Ellipse and IconEllipse components feat(index.ts): export Drawer component from drawer folder feat(index.ts): export IconEllipse component from icon-ellipse folder

---
+++ front/src/components/molecules/drawer/Drawer.tsx
@@ -0,0 +1,16 @@
+import {Ellipse} from "@/components/atoms";
+import {IconEllipse} from "@/components/molecules";
+
+export function Drawer(){
+    return (
+        <div>
+            <Ellipse bgColor={"#f0f0f0"}/>
+            <Ellipse bgColor={"#f0f0f0"}/>
+            <Ellipse bgColor={"#f0f0f0"}/>
+            <Ellipse bgColor={"#f0f0f0"}/>
+            <Ellipse bgColor={"#f0f0f0"}/>
+            <IconEllipse bgColor={"#f0f0f0"}/>
+        </div>
+
+    );
+}
---
+++ front/src/components/molecules/index.ts
@@ -0,0 +1,2 @@
+﻿export * from './drawer';
+export * from './icon-ellipse';
