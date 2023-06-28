da4fa3a163622b2fd74906807bd6bcc6a57850a4
KingDeadKnight <15180531+KingDeadKnight@users.noreply.github.com>
2023-06-28 23:58:55 +0200

    feat: made drawer component

--- front/.storybook/preview.ts
+++ front/.storybook/preview.ts
@@ -2,6 +2,30 @@ import '@/styles/globals.scss';
 
 import type { Preview } from '@storybook/react';
 
+const customViewports = {
+  Mobile: {
+    name: 'Mobile',
+    styles: {
+      width: '375px',
+      height: '812px',
+    },
+  },
+  Tablet: {
+    name: 'Tablet',
+    styles: {
+      width: '744px',
+      height: '1133px',
+    },
+  },
+  Desktop: {
+    name: 'Desktop',
+    styles: {
+      width: '1280px',
+      height: '708px',
+    },
+  },
+};
+
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
@@ -10,6 +34,19 @@ const preview: Preview = {
         color: /(background|color)$/i,
         date: /Date$/,
       },
+    },
+    backgrounds: {
+      default: 'light',
+      values: [
+        {
+          name: 'light',
+          value: '#F9F8FF',
+        },
+      ],
+    },
+    viewport: {
+      viewports: customViewports,
+      defaultViewport: 'Mobile'
     },
   },
 };
--- front/src/components/molecules/drawer/Drawer.tsx
+++ front/src/components/molecules/drawer/Drawer.tsx
@@ -3,14 +3,16 @@ import {IconEllipse} from "@/components/molecules";
 
 export function Drawer(){
     return (
-        <div>
-            <Ellipse bgColor={"#b31e1e"}/>
-            <Ellipse bgColor={"#f0f0f0"}/>
-            <Ellipse bgColor={"#f0f0f0"}/>
-            <Ellipse bgColor={"#26317f"}/>
-            <Ellipse bgColor={"#f0f0f0"}/>
-            <IconEllipse bgColor={"#f0f0f0"}/>
+        <div className='drawer'>
+            <span className='title'>Étagère 1</span>
+            <div className='drawer-container'>
+                <Ellipse bgColor="#9B1A30"/>
+                <Ellipse bgColor="#FAE7C0"/>
+                <Ellipse bgColor="#D18080"/>
+                <Ellipse bgColor="#D4B98A"/>
+                <Ellipse bgColor="#C6BBFF"/>
+                <IconEllipse bgColor="#f0f0f0" iconName='plus'/>
+            </div>
         </div>
-
     );
 }
